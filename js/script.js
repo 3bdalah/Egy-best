6aa8ee118c742edef3386d3dc4b4c9727800f238
3bdalah <3bdallah.dev@gmail.com>
2023-05-30 03:50:45 +0300

    Adding function tv-shows

--- js/script.js
+++ js/script.js
@@ -52,15 +52,61 @@ async function displayPopularMovies() {
 }
 
 
+
+
+
+async function displayPopularShows() {
+  const { results } = await fetchAPIData('tv/popular');
+
+  results.forEach((show) => {
+    const div = document.createElement('div');
+    div.classList.add('card');
+    div.innerHTML = `
+          <a href="tv-details.html?id=${show.id}">
+            ${
+              show.poster_path
+                ? `<img
+              src="https://image.tmdb.org/t/p/w500${show.poster_path}"
+              class="card-img-top"
+              alt="${show.title}"
+            />`
+                : `<img
+            src="../images/no-image.jpg"
+            class="card-img-top"
+            alt="${show.title}"
+          />`
+            }
+          </a>
+          <div class="card-body">
+            <h5 class="card-title">${show.name}</h5>
+            <p class="card-text">
+              <small class="text-muted">Release: ${show.first_air_date}</small>
+            </p>
+          </div>
+        `;
+
+    document.querySelector('#popular-shows').appendChild(div);
+  });
+}
+
 // fetch data from TMDB API 
 async function fetchAPIData(endpoint){
       const response = await fetch(`${global.api.apiUrl}${endpoint}?api_key=${global.api.apiKey}`);
-      
+      showSpinner();
       const data = await response.json();
-      console.log('response data ' , data);
+      hideSpinner();
       return data;
 }
 
+// show spinner 
+function showSpinner() {
+  document.querySelector('.spinner').classList.add('show');
+}
+
+// hide spinner 
+function  hideSpinner() {
+  document.querySelector('.spinner').classList.remove('show');
+}
 
 // hight list active link
 function hightActiveLink(){
@@ -82,7 +128,7 @@ function init(){
       break;
     case '/shows.html':
       console.log("shows");
-      displayPopularMovies();
+      displayPopularShows();
       break;
     case  '/movie-details.html':
       console.log("movie details");
