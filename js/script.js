5d2f93d32ec1b26eae493d4a34568360ee26087e
3bdalah <3bdallah.dev@gmail.com>
2023-05-30 14:33:34 +0300

    update script.js - add new function to handle display shows moives - add new function to handle movie details
    - add new function for handle  commas to number movie budget

--- js/script.js
+++ js/script.js
@@ -89,6 +89,75 @@ async function displayPopularShows() {
   });
 }
 
+// display movie details 
+async function displayMovieDetails(){
+  const movieId = window.location.search.split('=')[1];
+  const movie = await fetchMovie(`movie/${movieId}`);
+  const div = document.createElement('div');
+  div.innerHTML = `
+  <div class="details-top">
+  <div>
+  ${
+    movie.poster_path
+      ? `<img
+    src="https://image.tmdb.org/t/p/w500${movie.poster_path}"
+    class="card-img-top"
+    alt="${movie.title}"
+  />`
+      : `<img
+  src="../images/no-image.jpg"
+  class="card-img-top"
+  alt="${movie.title}"
+/>`
+  }
+  </div>
+  <div>
+    <h2>${movie.title}</h2>
+    <p>
+      <i class="fas fa-star text-primary"></i>
+      ${movie.vote_average.toFixed(1)} / 10
+    </p>
+    <p class="text-muted">Release Date: ${movie.release_date}</p>
+    <p>
+      ${movie.overview}
+    </p>
+    <h5>Genres</h5>
+    <ul class="list-group">
+      ${movie.genres.map((genre) => `<li>${genre.name}</li>`).join('')}
+    </ul>
+    <a href="${
+      movie.homepage
+    }" target="_blank" class="btn">Visit Movie Homepage</a>
+  </div>
+</div>
+<div class="details-bottom">
+  <h2>Movie Info</h2>
+  <ul>
+    <li><span class="text-secondary">Budget:</span> $${addCommasToNumber(
+      movie.budget
+    )}</li>
+    <li><span class="text-secondary">Revenue:</span> $${addCommasToNumber(
+      movie.revenue
+    )}</li>
+    <li><span class="text-secondary">Runtime:</span> ${
+      movie.runtime
+    } minutes</li>
+    <li><span class="text-secondary">Status:</span> ${movie.status}</li>
+  </ul>
+  <h4>Production Companies</h4>
+  <div class="list-group">
+    ${movie.production_companies
+      .map((company) => `<span>${company.name}</span>`)
+      .join(', ')}
+  </div>
+</div>
+  `;
+  document.querySelector('#movie-details').appendChild(div);
+}
+
+
+
+
 // fetch data from TMDB API 
 async function fetchAPIData(endpoint){
       const response = await fetch(`${global.api.apiUrl}${endpoint}?api_key=${global.api.apiKey}`);
@@ -118,6 +187,11 @@ function hightActiveLink(){
   });
 }
 
+
+// function add commas to number 
+function addCommasToNumber(number) {
+  return number.toString().replace(/\B(\d{3})+(?!\d)/g,',');
+}
 
 // init App 
 function init(){
